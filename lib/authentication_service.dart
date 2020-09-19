b08b8becd3570c7b2e6832aeb2f1629487b58232
robertbrunhage <robertbrunhage@gmail.com>
2020-09-19 19:47:04 +0200

    Add comments for service methods

--- lib/authentication_service.dart
+++ lib/authentication_service.dart
@@ -5,12 +5,20 @@ class AuthenticationService {
 
   AuthenticationService(this._firebaseAuth);
 
-  Stream<User> get authStateChanges => _firebaseAuth.authStateChanges();
+  /// Changed to idTokenChanges as it updates depending on more cases.
+  Stream<User> get authStateChanges => _firebaseAuth.idTokenChanges();
 
+  /// This won't pop routes so you could do something like
+  /// Navigator.of(context).pushNamedAndRemoveUntil('/', (Route<dynamic> route) => false);
+  /// after you called this method if you want to pop all routes.
   Future<void> signOut() async {
     await _firebaseAuth.signOut();
   }
 
+  /// There are a lot of different ways on how you can do exception handling.
+  /// This is to make it as easy as possible but a better way would be to
+  /// use your own custom class that would take the exception and return better
+  /// error messages. That way you can throw, return or whatever you prefer with that instead.
   Future<String> signIn({String email, String password}) async {
     try {
       await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
@@ -20,6 +28,10 @@ class AuthenticationService {
     }
   }
 
+  /// There are a lot of different ways on how you can do exception handling.
+  /// This is to make it as easy as possible but a better way would be to
+  /// use your own custom class that would take the exception and return better
+  /// error messages. That way you can throw, return or whatever you prefer with that instead.
   Future<String> signUp({String email, String password}) async {
     try {
       await _firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
